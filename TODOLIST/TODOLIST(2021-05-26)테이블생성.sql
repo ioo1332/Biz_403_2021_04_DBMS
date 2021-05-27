5480df2fb38cd6ea90631c44a425068b356cbd28
ioo1332 <ioo168400@gmail.com>
2021-05-27 16:38:47 +0900

    TODO

--- TODOLIST/TODOLIST(2021-05-26)테이블생성.sql
+++ TODOLIST/TODOLIST(2021-05-26)테이블생성.sql
@@ -9,8 +9,9 @@ create table tbl_todolist(
     td_stime varchar(10) not null, -- 추가된 시간
     td_doit varchar(300) not null,
     td_edate varchar(10) default '', -- 완료된 날짜
-    td_etime varbinary(10) default '' -- 완료된 시간
+    td_etime varchar(10) default '' -- 완료된 시간
 );
+drop table tbl_todolist;
 desc tbl_todolist;
 
 select *from tbl_todolist;
