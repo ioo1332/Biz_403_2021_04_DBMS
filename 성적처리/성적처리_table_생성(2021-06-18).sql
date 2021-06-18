83b6a358ef3cfbf5850dcfff70b823976354d9ea
ioo1332 <ioo168400@gmail.com>
2021-06-18 16:47:37 +0900

    score

---
+++ 성적처리/성적처리_table_생성(2021-06-18).sql
@@ -0,0 +1,28 @@
+create database score;
+use score;
+
+create table tbl_student(
+st_num	char(8)		primary key,
+st_name	nvarchar(20)	NOT NULL	,
+st_dept	nvarchar(20)	NOT NULL	,
+st_grade	bigint	NOT NULL	,
+st_tel	varchar(15)	NOT NULL	,
+st_addr	nvarchar(125)		
+);
+
+create table tbl_score(
+sc_seq	bigint auto_increment primary key,
+sc_stnum	char(8)	NOT NULL	,
+sc_subject	nvarchar(20)	NOT NULL	,
+sc_score	bigint	NOT NULL	
+);
+drop table tbl_score;
+
+alter table tbl_score
+add constraint fk_student
+foreign key (sc_stnum)
+references tbl_student(st_num);
+
+alter table tbl_score
+drop constraint fk_student;
+
